e347b02b282872f0259c9f87ff22ae2cc21432d5
Jorge Tapia <jorge@profapps.co>
2016-06-14 15:28:22 -0400

    Start parallax scrolling after 500 milliseconds.

--- main.lua
+++ main.lua
@@ -18,4 +18,7 @@ local layers = bgs[math.random(1, #bgs)]
 
 local parallax = require("parallax")
 parallax:init(layers, 8.0, 3.0)
-parallax:start()
+
+local startScrolling = function(event) parallax:start() end 
+timer.performWithDelay(500, startScrolling)
+
